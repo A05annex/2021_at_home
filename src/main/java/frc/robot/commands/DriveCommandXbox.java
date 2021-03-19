fb9be3d97a153b662e1b55485abbbc16c86ddb02
STHobbes <rhall@rubota.com>
2021-03-19 15:09:54 -0700

    Updated the Drive Command to match the drive_tune branch - should eliminate heading isolation.

--- src/main/java/frc/robot/commands/DriveCommandXbox.java
+++ src/main/java/frc/robot/commands/DriveCommandXbox.java
@@ -95,8 +95,11 @@ public class DriveCommandXbox extends CommandBase {
       NavX.HeadingInfo headingInfo = m_navx.getHeadingInfo();
       if (null != headingInfo) {
         rotation = (headingInfo.expectedHeading - headingInfo.heading) * Constants.DRIVE_ORIENTATION_kP;
-        rotation = Utl.clip(rotation,-0.5,0.5);
-//        rotation = 0.0;
+        // we noted that any time the robot stopped to wheels went to rotation only mode and started
+        // hunting for the expected heading. As soon as we started, all the wheels were in the wrong
+        // direction. Adding the speed multiplier (always positive, so it doesn't affect the direction)
+        // means that when the robot stops, the wheels are directed as expected.
+        rotation = Utl.clip(rotation,-0.5,0.5) * speed;
       } else {
         rotation = 0.0;
       }
@@ -110,7 +113,6 @@ public class DriveCommandXbox extends CommandBase {
     }
     // find direction, if the speed is 0 then it won't rotate
     double direction = Math.atan2(stickX, stickY);
-
     m_driveSubsystem.swerveDriveFieldRelative(direction, speed, rotation);
   }
 
